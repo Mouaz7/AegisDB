e0926e12ff1ee444ff9fa91f236cd45ff2bb0ac8
Mouaz Naji <moap23@student.bth.se>
2026-09-11 10:49:35 +0200

    Remove all sprint-related files

--- scripts/run-master-capstone-demo.sh
+++ scripts/run-master-capstone-demo.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "$DIR/run-sprint12-demo.sh" "$@"
+mvn test -pl aegisdb_integration -Dtest=MasterCapstoneIntegrationTest "$@"
